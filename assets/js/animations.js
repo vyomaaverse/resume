a9244e11b627cbc4630d9b5424d2d1c3afef8be5
Krati-Sharma-8 <83711754+Krati-Sharma-8@users.noreply.github.com>
2021-10-16 16:44:19 +0530

    Update animations.js

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -14,3 +14,14 @@ function myMove() {
     }
   }
 }
+
+
+id = setInterval(frame, 5);
+
+function frame() {
+  if (/* test for finished */) {
+    clearInterval(id);
+  } else {
+    /* code to change the element style */ 
+  }
+}
